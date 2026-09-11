1ffde782340c335c19029950b5e7d155c8023ab2
aslıgny <84908128+2021-asgny@users.noreply.github.com>
2026-09-12 01:47:58 +0300

    fix: update fact_funnel_dropoff with date and channel

--- 03_gold/fact_funnel_dropoff.sql
+++ 03_gold/fact_funnel_dropoff.sql
@@ -1,14 +1,18 @@
 CREATE OR REPLACE TABLE `my-project-0526-502020.Look_Ticaret_Gold.fact_funnel_dropoff` AS
 WITH session_steps AS (
   SELECT
+    event_date,
+    traffic_source AS channel_name,
     session_id,
     MAX(is_product_view) AS visited_product,
     MAX(is_cart_add) AS added_to_cart,
     MAX(is_purchase) AS purchased
   FROM `my-project-0526-502020.Look_Ticaret.stg_events`
-  GROUP BY session_id
+  GROUP BY event_date, traffic_source, session_id
 )
 SELECT
+  event_date,
+  channel_name,
   COUNT(session_id) AS total_sessions,
   COUNTIF(visited_product = 1) AS product_view_sessions,
   COUNTIF(added_to_cart = 1) AS cart_add_sessions,
@@ -17,4 +21,5 @@ SELECT
   ROUND(COUNTIF(visited_product = 1) * 100.0 / COUNT(session_id), 2) AS home_to_product_rate_pct,
   ROUND(COUNTIF(added_to_cart = 1) * 100.0 / NULLIF(COUNTIF(visited_product = 1), 0), 2) AS product_to_cart_rate_pct,
   ROUND(COUNTIF(purchased = 1) * 100.0 / NULLIF(COUNTIF(added_to_cart = 1), 0), 2) AS cart_to_purchase_rate_pct
-FROM session_steps;
+FROM session_steps
+GROUP BY event_date, channel_name;
