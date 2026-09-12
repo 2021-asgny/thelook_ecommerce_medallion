a1981b7e5e69632cc10d9f4c7bf6cbdba042e70f
aslıgny <84908128+2021-asgny@users.noreply.github.com>
2026-09-12 05:52:44 +0300

    feat: update fact_funnel_dropoff with event_id for product dimension mapping

--- 03_gold/fact_funnel_dropoff.sql
+++ 03_gold/fact_funnel_dropoff.sql
@@ -3,16 +3,18 @@ WITH session_steps AS (
   SELECT
     event_date,
     traffic_source AS channel_name,
+    event_id, 
     session_id,
     MAX(is_product_view) AS visited_product,
     MAX(is_cart_add) AS added_to_cart,
     MAX(is_purchase) AS purchased
   FROM `my-project-0526-502020.Look_Ticaret.stg_events`
-  GROUP BY event_date, traffic_source, session_id
+  GROUP BY event_date, traffic_source,event_id, session_id
 )
 SELECT
   event_date,
   channel_name,
+  event_id, 
   COUNT(session_id) AS total_sessions,
   COUNTIF(visited_product = 1) AS product_view_sessions,
   COUNTIF(added_to_cart = 1) AS cart_add_sessions,
@@ -22,4 +24,4 @@ SELECT
   ROUND(COUNTIF(added_to_cart = 1) * 100.0 / NULLIF(COUNTIF(visited_product = 1), 0), 2) AS product_to_cart_rate_pct,
   ROUND(COUNTIF(purchased = 1) * 100.0 / NULLIF(COUNTIF(added_to_cart = 1), 0), 2) AS cart_to_purchase_rate_pct
 FROM session_steps
-GROUP BY event_date, channel_name;
+GROUP BY event_date, channel_name, event_id;
